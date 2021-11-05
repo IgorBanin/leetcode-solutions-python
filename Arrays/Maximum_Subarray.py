6269c3028029052a20e5009719279976851b791c
IgorBanin <igor.banin@hotmail.com>
2021-11-05 10:28:08 -0700

    working solution of max contiguous subarray problem

--- Arrays/Maximum_Subarray.py
+++ Arrays/Maximum_Subarray.py
@@ -1,43 +1,16 @@
-from typing import List
-
-class Solution:
-
-    def maxSubArray(self, nums: List[int]) -> int:
-        copy = []
-        for num in nums:
-            copy.append(num)
-        # Handles the case that all numbers are negative
-        copy.sort()
-        print(copy)
-        if copy[len(copy) - 1] <= 0:
-            return copy[len(copy) - 1]
-
-        # Handles at least one non-negative number
-        sum = 0
-        max = nums[0]
-        for num in nums:
-            sum += num
-            if sum < 0:
-                sum = 0
-            elif sum > max:
-                max = sum
-
-        return max
-
-
-
+def maxSubArray(self, nums: List[int]) -> int:
+    max = nums[0]
+    sum = 0
+    for i in range(len(nums)):
+        sum = sum + nums[i]
+        if sum > max:
+            max = sum
+        if sum < 0:
+            sum = 0
+    return max
 
 def main():
-    test = [-2,1,-3,4,-1,2,1,-5,4]
-    test2 = [1]
-    test3 = [5,4,-1,7,8]
-    test4 = [-2,1]
-    test5 = [-2,-1, 0]
-    print("The max sum is", Solution().maxSubArray(test))
-    print("The max sum is", Solution().maxSubArray(test2))
-    print("The max sum is", Solution().maxSubArray(test3))
-    print("The max sum is", Solution().maxSubArray(test4))
-    print("The max sum is", Solution().maxSubArray(test5))
+    maxSubArray()
 
 if __name__ == "__main__":
     main()
